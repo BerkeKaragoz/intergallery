8e4f88d2448fa1125c5fededf98c8ce87b900cf6
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2022-05-14 20:09:25 +0300

    feat(web-ui): add LoadingButton

---
+++ apps/web-ui/src/components/LoadingButton/LoadingButton.tsx
@@ -0,0 +1,29 @@
+import { Button, ButtonProps, CircularProgress } from "@mui/material";
+
+type Props = ButtonProps & {
+  isLoading?: boolean;
+};
+
+const LoadingButton: React.FC<Props> = (props) => {
+  const { isLoading = false, children, ...rest } = props;
+
+  return (
+    <Button {...rest}>
+      {isLoading ? (
+        <>
+          <CircularProgress
+            color="inherit"
+            size={16}
+            sx={{ position: "absolute" }}
+          />
+          <div style={{ opacity: 0 }}>{children}</div>
+        </>
+      ) : (
+        children
+      )}
+    </Button>
+  );
+};
+
+export default LoadingButton;
+export type { Props as LoadingButtonProps };

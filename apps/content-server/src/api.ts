ea11374e036f9f59c67831c6657ad9ea756aae40
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2022-05-25 16:28:03 +0300

    feat(content-server): add send file in api

--- apps/content-server/src/api.ts
+++ apps/content-server/src/api.ts
@@ -1,10 +1,10 @@
 import { RequestHandler } from "express";
-import { dirname, resolve } from "path";
-import { SERVING_PATH } from "./index";
-import sharp from "sharp";
 import { existsSync, mkdirSync } from "fs";
+import { dirname, resolve } from "path";
+import sharp from "sharp";
+import { SERVING_PATH } from "./index";
 
-export const getFile: RequestHandler = (req, res) => {
+export const getFileThumb: RequestHandler = (req, res) => {
   const filePath = req.params[0];
   const absFilePath = resolve(SERVING_PATH, filePath);
   const absThumbPath = resolve(SERVING_PATH, "thumbnails", filePath);
@@ -27,4 +27,13 @@ export const getFile: RequestHandler = (req, res) => {
         res.sendFile(absThumbPath, { maxAge: "2 days" });
       });
   else res.sendFile(absThumbPath, { maxAge: "2 days" });
+};
+
+export const getFile: RequestHandler = (req, res) => {
+  const filePath = req.params[0];
+  const absFilePath = resolve(SERVING_PATH, filePath);
+
+  if (!existsSync(absFilePath))
+    res.sendFile(absFilePath, (err) => err && res.sendStatus(500));
+  else res.sendStatus(404);
 };
--- apps/content-server/src/router.ts
+++ apps/content-server/src/router.ts
@@ -3,6 +3,7 @@ import * as api from "./api";
 
 const router = Router();
 
-router.get("/thumb/*", api.getFile);
+router.get("/thumb/*", api.getFileThumb);
+router.get("/file/*", api.getFile);
 
 export default router;
